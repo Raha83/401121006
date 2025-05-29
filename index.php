ac376ccef919fff9f19cc11d97e69cd59a65f4b7
fBinesh <fatemehbinesh17@gmail.com>
2025-05-29 15:47:36 +0330

    feat:webProjuct structure

--- index.php
+++ index.php
@@ -1,45 +1,23 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Site</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
- </head>
- <body>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">main page</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/web/infs.php">information page</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/web/contact-us.php">contact us</a>
-        </li>
-       
-      </ul>
-    </div>
-  </div>
-</nav>
-<section>
-    <h1>First section of sending information</h1>
-    <form action="process-form.php" method="POST">
-        <div class="form-group">
-            <label for="name">name</label>
-            <input type="text" class="form-input" name="name" id="name" value="">
-        </div>
-        <div class="form-group">
-            <label for="name">last name</label>
-            <input type="text" class="form-input" name="name" id="name" value="">
-        </div>
-        <div class="form-group">
-             <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </form>
-</section>
-</body>
-</html>
+<?php
+define('ROOT_PATH',__DIR__);
+require __DIR__.'/vendor/autoload.php';
+require_once(__DIR__.'/helper/functions.php');
+require_once(__DIR__.'/app/Route.php');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+$config = require __DIR__.'/config/database.php';
+$capsule = new Capsule;
+$capsule->addConnection($config);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
+
+use App\Route;
+use App\Controller\FrontController;
+
+$route = new Route();
+$route->addRoute("GET",'/web2/',[FrontController::class, 'home']);
+$route->addRoute("GET",'/web2/about',[FrontController::class, 'about']);
+$route->addRoute("GET",'/web2/infs',[FrontController::class, 'infs']);
+$route->dispatch();
